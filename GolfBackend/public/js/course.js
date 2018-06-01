47cd527dff543f8e1b4fa0c8e96ef35240e72672
AaronCur <C00202505@itcarlow.ie>
2018-06-01 11:20:22 +0100

    Connected backend with frontend getting values form user input storing to database instead of predefined values

---
+++ GolfBackend/public/js/course.js
@@ -0,0 +1,6 @@
+function submit()
+{
+  var courseName = document.getElementById("course_name_field").value;
+  var numOfHoles = document.getElementById("holes").value;
+  init(courseName, numOfHoles);
+}
